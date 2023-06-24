b1c92d1fc287d9004a3232b9e7c68d9faa28d785
AnJimenezM <136032517+AnJimenezM@users.noreply.github.com>
2023-06-24 12:56:08 -0500

    Add files via upload

---
+++ Listado_numeros.js
@@ -0,0 +1,202 @@
+
+
+
+alert("ingresar 10 numeros correspondientes a la edad:");
+
+ listado_numeros = []
+
+ listado_numeros [0] = parseInt(prompt("numero01: "));
+ listado_numeros [1] = parseInt(prompt("numero02: "));
+ listado_numeros [2] = parseInt(prompt("numero03: "));
+ listado_numeros [3] = parseInt(prompt("numero04: "));
+ listado_numeros [4] = parseInt(prompt("numero05: "));
+ listado_numeros [5] = parseInt(prompt("numero06: "));
+ listado_numeros [6] = parseInt(prompt("numero07: "));
+ listado_numeros [7] = parseInt(prompt("numero08: "));
+ listado_numeros [8] = parseInt(prompt("numero09: "));
+ listado_numeros [9] = parseInt(prompt("numero010: "));
+
+ var menorEdad = 0;
+ var mayorEdad = 0;
+ var adultoEdad = 0;
+ var datoInv = 0;
+
+ // se aplicara las siguientes condiciones
+
+ //listado_numeros [0]
+
+ if (listado_numeros[0] >= 120){
+    datoInv = datoInv + 1;
+    listado_numeros[0] = null;
+ } else if (listado_numeros[0] < 18){
+    menorEdad = menorEdad + 1;
+ } else if (listado_numeros[0] >= 18 && listado_numeros[0] < 60){
+    mayorEdad = mayorEdad + 1;
+ } else if (listado_numeros[0] >= 60){
+    mayorEdad = mayorEdad +1;
+    adultoEdad = adultoEdad +1;
+ }
+
+ //listado_numeros[1]
+
+ if(listado_numeros[1 < 120]){
+    datoInv = datoInv +1;
+    listado_numeros[1] = null;
+ } else if (listado_numeros[1] < 18){
+    menorEdad = menorEdad + 1;
+ } else if (listado_numeros[1]>= 18 && listado_numeros[1] < 60){
+    mayorEdad = mayorEdad +1;
+ } else if (listado_numeros[1] >= 60){
+    mayorEdad = mayorEdad + 1;
+    adultoEdad = adultoEdad + 1;
+ }
+
+ //listado_numeros[2]
+
+ if(listado_numeros[2 < 120]){
+    datoInv = datoInv +1;
+    listado_numeros[2] = null;
+ } else if (listado_numeros[2] < 18){
+    menorEdad = menorEdad + 1;
+ } else if (listado_numeros[2]>= 18 && listado_numeros[1] < 60){
+    mayorEdad = mayorEdad +1;
+ } else if (listado_numeros[2] >= 60){
+    mayorEdad = mayorEdad + 1;
+    adultoEdad = adultoEdad + 1;
+ }
+
+ //listado_numeros[3]
+ if(listado_numeros[3 < 120]){
+    datoInv = datoInv +1;
+    listado_numeros[3] = null;
+ } else if (listado_numeros[3] < 18){
+    menorEdad = menorEdad + 1;
+ } else if (listado_numeros[3]>= 18 && listado_numeros[1] < 60){
+    mayorEdad = mayorEdad +1;
+ } else if (listado_numeros[3] >= 60){
+    mayorEdad = mayorEdad + 1;
+    adultoEdad = adultoEdad + 1;
+ }
+ //listado_numeros[4]
+
+ if(listado_numeros[4 < 120]){
+    datoInv = datoInv +1;
+    listado_numeros[4] = null;
+ } else if (listado_numeros[4] < 18){
+    menorEdad = menorEdad + 1;
+ } else if (listado_numeros[4]>= 18 && listado_numeros[1] < 60){
+    mayorEdad = mayorEdad +1;
+ } else if (listado_numeros[4] >= 60){
+    mayorEdad = mayorEdad + 1;
+    adultoEdad = adultoEdad + 1;
+ }
+ //listado_numeros[5]
+
+ if(listado_numeros[5 < 120]){
+    datoInv = datoInv +1;
+    listado_numeros[5] = null;
+ } else if (listado_numeros[5] < 18){
+    menorEdad = menorEdad + 1;
+ } else if (listado_numeros[5]>= 18 && listado_numeros[1] < 60){
+    mayorEdad = mayorEdad +1;
+ } else if (listado_numeros[5] >= 60){
+    mayorEdad = mayorEdad + 1;
+    adultoEdad = adultoEdad + 1;
+ }
+ //listado_numeros[6]
+
+ if(listado_numeros[6 < 120]){
+    datoInv = datoInv +1;
+    listado_numeros[6] = null;
+ } else if (listado_numeros[6] < 18){
+    menorEdad = menorEdad + 1;
+ } else if (listado_numeros[6]>= 18 && listado_numeros[1] < 60){
+    mayorEdad = mayorEdad +1;
+ } else if (listado_numeros[6] >= 60){
+    mayorEdad = mayorEdad + 1;
+    adultoEdad = adultoEdad + 1;
+ }
+ //listado_numeros[7]
+
+ if(listado_numeros[7 < 120]){
+    datoInv = datoInv +1;
+    listado_numeros[7] = null;
+ } else if (listado_numeros[7] < 18){
+    menorEdad = menorEdad + 1;
+ } else if (listado_numeros[7]>= 18 && listado_numeros[1] < 60){
+    mayorEdad = mayorEdad +1;
+ } else if (listado_numeros[7] >= 60){
+    mayorEdad = mayorEdad + 1;
+    adultoEdad = adultoEdad + 1;
+ }
+
+ //listado_numeros[8]
+
+ if(listado_numeros[8 < 120]){
+    datoInv = datoInv +1;
+    listado_numeros[8] = null;
+ } else if (listado_numeros[8] < 18){
+    menorEdad = menorEdad + 1;
+ } else if (listado_numeros[8]>= 18 && listado_numeros[1] < 60){
+    mayorEdad = mayorEdad +1;
+ } else if (listado_numeros[8] >= 60){
+    mayorEdad = mayorEdad + 1;
+    adultoEdad = adultoEdad + 1;
+ }
+ //listado_numeros[9]
+ if(listado_numeros[9 < 120]){
+    datoInv = datoInv +1;
+    listado_numeros[9] = null;
+ } else if (listado_numeros[9] < 18){
+    menorEdad = menorEdad + 1;
+ } else if (listado_numeros[9]>= 18 && listado_numeros[1] < 60){
+    mayorEdad = mayorEdad +1;
+ } else if (listado_numeros[9] >= 60){
+    mayorEdad = mayorEdad + 1;
+    adultoEdad = adultoEdad + 1;
+ }
+
+  
+
+  promedio = (listado_numeros[0] + 
+  listado_numeros[1] +
+  listado_numeros[2] +
+  listado_numeros[3] +
+  listado_numeros[4] +
+  listado_numeros[5] +
+  listado_numeros[6] +
+  listado_numeros[7] +
+  listado_numeros[8] +
+  listado_numeros[9]) /10;
+
+  document.write("hay: " + mayorEdad + "mayor(es) de edad. <br>");
+  document.write("hay: " + menorEdad + "menor(es) de edad. <br>");
+  document.write("hay: " + adultoEdad + "adulto mayor(es) de edad. <br>");
+  document.write("el promedio de edades ingresadas es de: " + promedio + "años. <br>");
+  alert("datos invalidos de edades en la lista" + datoInv);
+
+  maximo = Math.max(
+ listado_numeros[1], 
+  listado_numeros[2], 
+  listado_numeros[3], 
+  listado_numeros[4],
+  listado_numeros[5],
+  listado_numeros[6], 
+  listado_numeros[7], 
+  listado_numeros[8], 
+  listado_numeros[9],
+  );
+
+  minimo = Math.min(
+  listado_numeros[1], 
+  listado_numeros[2], 
+  listado_numeros[3], 
+  listado_numeros[4],
+  listado_numeros[5],
+  listado_numeros[6], 
+  listado_numeros[7], 
+  listado_numeros[8], 
+  listado_numeros[9],
+  );
+
+  
